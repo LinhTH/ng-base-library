b6267a438a41a9659ac5066b8a9d03f5abb598cc
Linh Truong (extern) (U806345) <linh.truong@mobi.ch>
2020-12-04 17:53:32 +0700

    feat: export 3 sub entries' public-apis

--- projects/base-lib/src/public-api.ts
+++ projects/base-lib/src/public-api.ts
@@ -1,3 +1,7 @@
 /*
  * Public API Surface of base-lib
  */
+
+export * from '@base/base-lib/src/lib/feature-a';
+export * from '@base/base-lib/src/lib/feature-b';
+export * from '@base/base-lib/src/lib/feature-c';
