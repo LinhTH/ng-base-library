54d54a67f8963a6b210318b2538209d48a786d78
Linh Truong (extern) (U806345) <linh.truong@mobi.ch>
2020-12-04 17:36:14 +0700

    chore: change the path and delete default components

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 ```shell script
 ng new ng-base-library --createApplication false --prefix base-lib
 ```
+2. Add libraty
+```shell script
+ng g library base-lib --prefix base-lib
+```
--- projects/base-lib/src/public-api.ts
+++ projects/base-lib/src/public-api.ts
@@ -1,7 +1,3 @@
 /*
  * Public API Surface of base-lib
  */
-
-export * from './lib/base-lib.service';
-export * from './lib/base-lib.component';
-export * from './lib/base-lib.module';
