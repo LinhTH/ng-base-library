fdf522823cd32cefbac579b9ba113949842136f4
Linh Truong (extern) (U806345) <linh.truong@mobi.ch>
2020-12-04 17:53:12 +0700

    feat: use ng-samurai semantic to generate 1 sub-entrie

---
+++ projects/base-lib/src/lib/feature-c/feature-c.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'base-lib-feature-c',
+  templateUrl: './feature-c.component.html',
+  styleUrls: ['./feature-c.component.css']
+})
+export class FeatureCComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/base-lib/src/lib/feature-c/feature-c.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FeatureCComponent } from './feature-c.component';
+
+
+
+@NgModule({
+  declarations: [FeatureCComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FeatureCModule { }
